25487a4ed6bcb4d6c2c627e1ef8823f93cc5a395
ashleyk1261 <ashleyk@udel.edu>
2017-10-10 17:53:03 -0400

    Corrected version
    Uploaded wrong ones originally

--- FlamingoBLL/src/main/java/pkgCore/Deck.java
+++ FlamingoBLL/src/main/java/pkgCore/Deck.java
@@ -2,13 +2,17 @@ package pkgCore;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.stream.Collectors;
+
+import org.apache.commons.math3.exception.OutOfRangeException;
 
 import pkgEnum.eRank;
 import pkgEnum.eSuit;
+import pkgException.DeckException;
 
 public class Deck {
 
-	private ArrayList<Card> cardsInDeck;
+	private ArrayList<Card> cardsInDeck = new ArrayList<Card>();
 
 	public Deck() {
 		for (eSuit eSuit : eSuit.values()) {
@@ -19,10 +23,65 @@ public class Deck {
 		Collections.shuffle(cardsInDeck);
 	}
 
-	public Card Draw() {
-
+	// Method throws an exception if the deck is empty
+	public Card Draw() throws DeckException {
+		if (cardsInDeck.size() == 0) {
+			throw new DeckException(this);
+		}
 		return cardsInDeck.remove(0);
-
 	}
+
+	// An overloaded Draw method that draws a card of a given eSuit
+	public Card Draw(eSuit eSuit) throws DeckException {
+		if (cardsInDeck.size() == 0) {
+			throw new DeckException(this);
+		} else {
+			if (Count(eSuit) != 0) {
+				ArrayList<Card> subsetOfCards = cardsInDeck.stream().filter(deckCards -> deckCards.geteSuit() == eSuit)
+						.collect(Collectors.toCollection(ArrayList::new));
+				cardsInDeck.remove(subsetOfCards.get(0));
+				return subsetOfCards.get(0);
+			} else
+				return null;
+		}
+	}
+
+	// An overloaded Draw method that draws a card of a given eRank
+	public Card Draw(eRank eRank) throws DeckException {
+		if (cardsInDeck.size() == 0) {
+			throw new DeckException(this);
+		} else {
+			if (Count(eRank) != 0) {
+				ArrayList<Card> subsetOfCards = cardsInDeck.stream().filter(deckCards -> deckCards.geteRank() == eRank)
+						.collect(Collectors.toCollection(ArrayList::new));
+				cardsInDeck.remove(subsetOfCards.get(0));
+				return subsetOfCards.get(0);
+			} else
+				return null;
+		}
+	}
+
+	// A method that returns the number of a given eSuit left in the deck.
+	public int Count(eSuit eSuit) {
+		ArrayList<Card> subsetOfCards = cardsInDeck.stream().filter(deckCards -> deckCards.geteSuit() == eSuit)
+				.collect(Collectors.toCollection(ArrayList::new));
+		return subsetOfCards.size();
+	}
+
+	// A method that returns the number of a given eRank left in the deck.
+	public int Count(eRank eRank) {
+		ArrayList<Card> subsetOfCards = cardsInDeck.stream().filter(deckCards -> deckCards.geteRank() == eRank)
+				.collect(Collectors.toCollection(ArrayList::new));
+		return subsetOfCards.size();
+	}
+
+	// A method that returns 0 or 1 if a given card is left in the deck.
+	public int Count(Card card) {
+		ArrayList<Card> subsetOfCards = cardsInDeck.stream().filter(
+				deckCards -> ((deckCards.geteRank() == card.geteRank()) && (deckCards.geteSuit() == card.geteSuit())))
+				.collect(Collectors.toCollection(ArrayList::new));
+		return subsetOfCards.size();
+	}
+
 
 }
--- FlamingoBLL/src/test/java/pkgCore/DeckTest.java
+++ FlamingoBLL/src/test/java/pkgCore/DeckTest.java
@@ -3,14 +3,140 @@ package pkgCore;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import pkgException.DeckException;
+
+import pkgEnum.eRank;
+import pkgEnum.eSuit;
 
 public class DeckTest {
 
 	@Test
-	public void TestDeck() {
-		
-		//	TODO: Build a deck(1), make sure there are 52 cards in the deck
-		//	TODO: Build a deck(6), make sure there are 312 cards in the deck 
+	public void TestEmptyDeck() {
+		// TODO: Build a deck, draw until you get a DeckException
+		try {
+			Deck AKDeck = new Deck();
+			for (int i = 1; i <= 54; i++) {
+				AKDeck.Draw();
+			}
+		} catch (DeckException ex) {
+			System.out.println("The deck is empty.  No more cards can be drawn.");
+		}
+
+	}
+
+	@Test
+	public void TestDrawSuit() {
+		// TODO: Build a deck, test the Draw(eSuit) method
+		try {
+			Deck AKDeck = new Deck();
+			assertEquals(eSuit.DIAMONDS, AKDeck.Draw(eSuit.DIAMONDS).geteSuit());
+		} catch (DeckException ex) {
+			System.out.println("The deck is empty.  No more cards can be drawn.");
+		}
+	}
+
+	@Test
+	public void TestDrawRank() {
+		// TODO: Build a deck, test the Draw(eRank) method
+		try {
+			Deck AKDeck = new Deck();
+			assertEquals(eRank.FOUR, AKDeck.Draw(eRank.FOUR).geteRank());
+		} catch (DeckException ex) {
+			System.out.println("The deck is empty.  No more cards can be drawn.");
+		}
+	}
+
+	@Test
+	public void TestDeckRankCount() {
+		// TODO: Build a deck, test the DeckRankCount method
+		try {
+			Deck AKDeck = new Deck();
+			for (int i = 4; i >= 0; i--) {
+				assertEquals(i, AKDeck.Count(eRank.ACE));
+				AKDeck.Draw(eRank.ACE);
+			}
+		} catch (DeckException ex) {
+			System.out.println("The deck is empty.  No more cards can be drawn.");
+		}
+	}
+
+	@Test
+	public void TestDeckSuitCount() {
+		// TODO: Build a deck, test the DeckSuitCount method
+		try {
+			Deck AKDeck = new Deck();
+			for (int i = 13; i >= 0; i--) {
+				assertEquals(i, AKDeck.Count(eSuit.HEARTS));
+				AKDeck.Draw(eSuit.HEARTS);
+			}
+		} catch (DeckException ex) {
+			System.out.println("The deck is empty.  No more cards can be drawn.");
+		}
+	}
+
+	@Test
+	public void TestDeckCount() {
+		Deck AKDeck = new Deck();
+		Card testCard = new Card(eSuit.CLUBS, eRank.ACE);
+		assertEquals(1, AKDeck.Count(testCard));
+	}
+
+	@Test
+	public void TestDeckCountNoMatch() {
+		try {
+			Deck AKDeck = new Deck();
+			Card testCard = new Card(eSuit.CLUBS, eRank.ACE);
+			for (int i = 5; i >= 0; i--) {
+				AKDeck.Draw(eRank.ACE);
+			}
+			assertEquals(0, AKDeck.Count(testCard));
+		} catch (DeckException ex) {
+			System.out.println("The deck is empty.  No more cards can be drawn.");
+		}
+	}
+
+	@Test
+	public void TestDeckCountNoMatch2() {
+		try {
+			Deck AKDeck = new Deck();
+			Card testCard = new Card(eSuit.CLUBS, eRank.ACE);
+			for (int i = 14; i >= 0; i--) {
+				AKDeck.Draw(eSuit.CLUBS);
+			}
+			assertEquals(0, AKDeck.Count(testCard));
+		} catch (DeckException ex) {
+			System.out.println("The deck is empty.  No more cards can be drawn.");
+		}
+	}
+
+	@Test
+	public void TestDeckDrawFromEmptyDeck() {
+		try {
+			Deck AKDeck = new Deck();
+			Card testCard = new Card(eSuit.CLUBS, eRank.ACE);
+			for (int i = 52; i >= 1; i--) {
+				AKDeck.Draw();
+			}
+			AKDeck.Draw(eSuit.CLUBS);
+			assertEquals(0, AKDeck.Count(testCard));
+		} catch (DeckException ex) {
+			System.out.println("The deck is empty.  No more cards can be drawn.");
+		}
+	}
+
+	@Test
+	public void TestDeckDrawFromEmptyDeck2() {
+		try {
+			Deck AKDeck = new Deck();
+			Card testCard = new Card(eSuit.CLUBS, eRank.ACE);
+			for (int i = 52; i >= 1; i--) {
+				AKDeck.Draw();
+			}
+			AKDeck.Draw(eRank.THREE);
+			assertEquals(0, AKDeck.Count(testCard));
+		} catch (DeckException ex) {
+			System.out.println("The deck is empty.  No more cards can be drawn.");
+		}
 	}
 
 }
